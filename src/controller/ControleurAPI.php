df46a43c3fee5706785b162c7bc98cc6ddd36964
Alessi <alessi.demange2@etu.univ-lorraine.fr>
2021-03-30 17:30:36 +0200

    Pagination

--- src/controller/ControleurAPI.php
+++ src/controller/ControleurAPI.php
@@ -2,7 +2,7 @@
 namespace seance\controller;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
+use seance\model\Game;
 use seance\vues;
 class ControleurAPI {
     private $container;
@@ -12,13 +12,41 @@ class ControleurAPI {
     }
 
     public function generateObjAPI(Request $rq, Response $rs, $args) {
-        $listeJeu = Game::where('id','=',(int) $args['id'])->firstOrFail();
+        $listeJeu = Game::select("id","name","alias","deck","description","original_release_date")->where('id','=',(int) $args['id'])->firstOrFail();
         $tableau = json_encode($listeJeu, JSON_FORCE_OBJECT);
-        $vue = new Vue([], $this->container);
-        //echo "eho";
+        $vue = new vues\Vue([$tableau], $this->container);
 
         $rs->getBody()->write($vue->render(0));
-        //$rs->getBody()->write("28");
         return $rs;
     }
+
+    public function affichageJeux(Request $rq, Response $rs, $args) {
+        $page = false;
+        if($_GET != null){
+            $page = $_GET['page'];
+        }
+        if(!$page || $page==1){
+            $listeJeux = Game::select("id","name","alias","deck")->limit(200)->get();
+            $pagePlus = $page + 1;
+            $pageMoins = $page - 1;
+            $links = array("next" => array("href" => "/api/games?page={$pagePlus}"));
+        }elseif ($page==240){
+            $listeJeux = Game::select("id","name","alias","deck")->limit(200)->get();
+            $pagePlus = $page + 1;
+            $pageMoins = $page - 1;
+            $links = array("prev" => array("href" => "/api/games?page={$pageMoins}"));
+        }
+        else{
+            $listeJeux = Game::select("id","name","alias","deck")->offset(200*($page-1))->limit(200)->get();
+            $pagePlus = $page + 1 ;
+            $pageMoins = $page - 1;
+            $links = array("prev" => array("href" => "/api/games?page={$pageMoins}"), "next" => array("href" => "/api/games?page={$pagePlus}"));
+        }
+        $array = ["games" => $listeJeux, "links" => $links];
+        $tableau = json_encode($array, JSON_UNESCAPED_SLASHES);
+        $vue = new vues\Vue([$tableau], $this->container);
+        $rs->getBody()->write($vue->render(1));
+        return $rs;
+    }
+
 }
--- src/vues/Vue.php
+++ src/vues/Vue.php
@@ -25,16 +25,9 @@ class Vue {
     }
 
     /**
-     * Affichage des listes publiques
-     * @return string
+     * Fonctions
      */
-    public function listesPublique() : string{
 
-    }
-
-    public function listeCreateur() : string{
-
-    }
     /**
      * RENDER
      * @param int $select
@@ -42,28 +35,29 @@ class Vue {
      */
     public function render( int $select ) : string
     {
+        $content =  "";
         switch ($select) {
+            //Affichage 1 jeux
             case 0 :
             {
-                $this->container = "<h2>blabla</h2>";
+                $content .= $this->tab[0];
+                break;
+            }
+            //Affichage 200 jeux
+            case 1 :
+            {
+                $content .= $this->tab[0];
                 break;
             }
         }
-
-
         $html = <<<FIN
 <!DOCTYPE html>
 <html>
 <head>
-    <title>MyWishList</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <title>GamesPedia</title>
 </head>
 <body>
-<p>ui</p>>    
+$content   
 </body>
 </html>
 FIN;
