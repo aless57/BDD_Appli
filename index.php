0883d27f3bb0316089ba3239210468a08825e1a6
Aless <72187288+aless57@users.noreply.github.com>
2021-03-30 18:01:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+session_start();
+
+use seance\controller\ControleurAPI;
+
+require 'vendor/autoload.php';
+
+
+$config = ['settings' => [
+    'displayErrorDetails' => true,
+]];
+$container = new \Slim\Container($config);
+
+$db = new \Illuminate\Database\Capsule\Manager();
+$db->addConnection(parse_ini_file('src/conf/db.config.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$container = new \Slim\Container($config);
+$app = new \Slim\App($container);
+
+
+//Chemin Accueil
+$app->get('/api/games/{id}', ControleurAPI::class.':generateObjAPI')->setName('affichageUnJeu');
+$app->get('/api/games', ControleurAPI::class.':affichageJeux')->setName('affichage200Jeu');
+
+$app->run();
